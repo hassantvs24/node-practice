3a38d2cbb329f5392ea7eeda88314136798dc44c
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-03-10 21:59:00 +0600

    Error Handle

--- vidly/index.js
+++ vidly/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 require('express-async-errors');
+const winston = require('winston');
+require('winston-mongodb');
 const error = require('./middleware/error');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
@@ -16,6 +18,27 @@ Joi.objectId = require('joi-objectid')(Joi);
 
 const port = process.env.PORT || 3000;
 
+process.on('uncaughtException', (ex) => { //Uncaught Exceptions Error Handle
+    //console.log('We got an Uncaught Exception.');
+    winston.error(ex.message, ex);
+    process.exit(1);//1 = process exit active 0= process exit inactive
+});
+
+process.on('unhandledRejection', (ex) => { //Unhandled Rejection Error Handle
+    throw ex;
+    //console.log('We got an Unhandled Rejection.');
+    //winston.error(ex.message, ex);
+    //process.exit(1);//1 = process exit active 0= process exit inactive
+});
+
+winston.exceptions.handle(new winston.transports.File({filename: 'uncaughtException.log'}));//File base error log implementation for exception
+winston.add(new winston.transports.File({filename: 'logfile.log'}));//File base error log implementation
+winston.add(new winston.transports.MongoDB({db: 'mongodb://localhost/vidly', options: {useUnifiedTopology: true, useNewUrlParser: true}}));//File base error log implementation
+
+
+const p = Promise.reject(new Error('Unhandled Rejection during startup.'));
+p.then(() => console.log('Done'));
+//throw new Error('Failed during startup.');
 
 if(!config.get('jwtPrivateKey')){
     console.error('FATAL ERROR: jwtPrivateKey is not defined.');
